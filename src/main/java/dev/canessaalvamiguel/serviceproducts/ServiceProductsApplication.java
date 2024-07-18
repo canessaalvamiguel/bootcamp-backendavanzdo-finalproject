ee4541a7785a3f9a55f37d5420e6b016d89a3084
Miguel Canessa <canessaalvamiguel@gmail.com>
2024-07-17 19:12:37 -0500

    Init commit

---
+++ src/main/java/dev/canessaalvamiguel/serviceproducts/ServiceProductsApplication.java
@@ -0,0 +1,13 @@
+package dev.canessaalvamiguel.serviceproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProductsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceProductsApplication.class, args);
+  }
+
+}
